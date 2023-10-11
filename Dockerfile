ae5935425ba6bad546b40404a3619ce6001195b4
lecongdien2000 <18130038@st.hcmuaf.edu.vn>
2023-10-11 21:32:55 +0700

    Added my project

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM eclipse-temurin:17-jdk-alpine
+VOLUME /tmp
+COPY build/libs/*.jar spring-db-first-application-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
+EXPOSE 8080
--- src/main/java/com/example/SpringDbFirstApplication.java
+++ src/main/java/com/example/SpringDbFirstApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
+
 public class SpringDbFirstApplication {
 
 	public static void main(String[] args) {
@@ -14,3 +15,4 @@ public class SpringDbFirstApplication {
 	}
 
 }
+
